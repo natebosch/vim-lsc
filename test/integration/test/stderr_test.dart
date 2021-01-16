bf5a0765cf54dfe73197c807d8c0d0ab90452943
Nate Bosch <nbosch1@gmail.com>
2021-01-16 10:24:18 -0800

    Use a regex to check vim message (#367)
    The exact message format seems to vary based on versions of vim, or
    possibly flakily based on prior interactions. In either case, check the
    message against a regex rather than an exact full match to allow some
    variation which does not impact the intent of the test.

--- test/integration/test/stderr_test.dart
+++ test/integration/test/stderr_test.dart
@@ -45,7 +45,7 @@ void main() {
     }
     final messages = await vim.messages(2);
     expect(messages, [
-      '"foo.txt" [New] --No lines in buffer--',
+      matches('"foo.txt" .* --No lines in buffer--'),
       '[lsc:Error] Failed to initialize server "some server". '
           'Failing command is: [\'sh\', \'-c\', \'echo messagestderr >&2\']'
     ]);
